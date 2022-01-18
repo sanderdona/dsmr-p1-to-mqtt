894c90b96a940690375ceb09e7b62da82e463712
sanderdona <spdona@gmail.com>
2022-01-18 16:05:37 +0100

    Using a MQTT service to handle connecting, disconnecting and publishing

---
+++ reader/service/mqtt_service.py
@@ -0,0 +1,40 @@
+import logging
+
+import paho.mqtt.client as mqtt
+
+from reader.domain import MqttConfig, MqttMessage
+
+
+class MqttService:
+
+    def __init__(self, mqtt_config: MqttConfig):
+        self.client = mqtt.Client(mqtt_config.client_id)
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_log = self.on_log
+        self.client.username_pw_set(mqtt_config.username, mqtt_config.password)
+        try:
+            self.client.connect(mqtt_config.host, mqtt_config.port)
+        except ConnectionRefusedError as e:
+            logging.error(f"Failed to connect to {mqtt_config.host}:{mqtt_config.port} ({e.strerror})")
+            exit(1)
+
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            logging.info("Connected to MQTT broker")
+            logging.debug("Starting loop...")
+            self.client.loop_start()
+            logging.debug("Loop started")
+        else:
+            logging.warning("Failed to connect to MQTT broker. Return code: " + rc)
+
+    def on_disconnect(self):
+        logging.debug("Disconnected. Stopping loop...")
+        self.client.loop_stop()
+        logging.debug("Loop stopped")
+
+    def on_log(self, client, userdata, level, buf):
+        logging.debug("MQTT log: " + buf)
+
+    async def publish(self, message: MqttMessage):
+        logging.debug(f"Publishing payload {message.payload} to topic {message.topic}")
--- reader/telegram_to_mqtt.py
+++ reader/telegram_to_mqtt.py
@@ -3,7 +3,8 @@ import re
 from datetime import datetime
 import pytz
 
-import paho.mqtt.publish as publish
+from reader.domain import MqttMessage
+from reader.service import MqttService
 
 interesting_codes = {
     '0-0:1.0.0': {'topic': 'timestamp', 'type': 'timestamp'},
@@ -33,6 +34,7 @@ class TelegramToMqtt:
         logging.info(f"Publishing MQTT messages to host {self.mqtt_config.host} "
                      f"on port {self.mqtt_config.port} "
                      f"with client id {self.mqtt_config.client_id}")
+        self.mqtt_service = MqttService(self.mqtt_config)
 
     async def handle_new_telegram(self):
         telegram = self.serial_port.get_telegram()
@@ -41,19 +43,8 @@ class TelegramToMqtt:
         messages = self.convert_to_messages(telegram)
         logging.debug(f"Publishing messages to MQTT")
 
-        auth = {
-            'username': self.mqtt_config.username,
-            'password': self.mqtt_config.password
-        }
-
-        publish.multiple(
-            messages,
-            hostname=self.mqtt_config.host,
-            port=self.mqtt_config.port,
-            client_id=self.mqtt_config.client_id,
-            auth=auth
-        )
-
+        for message in messages:
+            await self.mqtt_service.publish(message)
         logging.info(f"Published {len(messages)} messages successfully")
 
     def convert_to_messages(self, telegram):
@@ -75,11 +66,7 @@ class TelegramToMqtt:
                 topic = interesting_codes[code]['topic']
                 value_type = interesting_codes[code]['type']
                 value = self.clean_value(telegram_row, value_type)
-                msg = {
-                    'topic': f"{self.mqtt_config.base_topic}{topic}",
-                    'payload': value
-                }
-                return msg
+                return MqttMessage(value, f"{self.mqtt_config.base_topic}{topic}")
 
     def clean_value(self, telegram_row, value_type):
         value = telegram_row[telegram_row.find('(') + 1: telegram_row.find(')')]
