8d2ed8bbe2a06e22387d8b35b360bbd7e00d01e2
sanderdona <spdona@gmail.com>
2024-01-12 15:17:52 +0100

    WIP

--- install.sh
+++ install.sh
@@ -2,35 +2,28 @@
 
 REPO_NAME="dsmr-p1-to-mqtt"
 
-# Create a directory, download the zip and extract.
-echo "Downloading files..."
+echo "Download and extract files..."
 mkdir "${REPO_NAME}"
 curl -L "https://github.com/sanderdona/${REPO_NAME}/archive/refs/heads/main.zip" -o "${REPO_NAME}/${REPO_NAME}.zip"
 unzip "${REPO_NAME}/${REPO_NAME}.zip" -d "${REPO_NAME}"
 rm "${REPO_NAME}/${REPO_NAME}.zip"
 echo "Done"
 
-#Installing python3-venv
+cd "${REPO_NAME}" || echo "Failed to create directory." ; exit
+
 echo "Installing python3-venv..."
 sudo apt-get install python3-venv
 echo "Done"
-
-# Make the p1-to-mqtt file executable.
-echo "Going into newly created dir $REPO_NAME"
-# shellcheck disable=SC2164
-cd "${REPO_NAME}"
 
 echo "Make service executable..."
 chmod +x bin/p1-to-mqtt
 echo "Done"
 
-# Create a virtual environment and activate it.
 echo "Create a virtual environment..."
 python3 -m venv .venv
 source .venv/bin/activate
 echo "Done"
 
-# Install required packages
 echo "Install required packages in virtual environment..."
 pip install .
 echo "Done"
